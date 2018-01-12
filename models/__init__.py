79f94cd01096fe372a14da20fdeb1a44a22fdcd2
ssabate <ssabate@iesebre.com>
2018-01-12 12:05:29 +0100

    Mes coflictes

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-<<<<<<< HEAD
-<<<<<<< HEAD
 from . import esports
-=======
-from . import esports
->>>>>>> 5a1d4240f9efcffdf18c8bdf0ee62355c55d0ce8
-=======
-from . import esports
->>>>>>> 267b13de6ca2f0be1a0d0d98e65487edd5d20122
+
